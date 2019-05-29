345585d29b359b192f22a3850a19d490eb9c47bb
Luiz Junior <inf.gonzaga@gmail.com>
2019-05-28 22:54:26 -0300

    regioes 1000

--- app/src/main/java/com/example/regioes/MainActivity.java
+++ app/src/main/java/com/example/regioes/MainActivity.java
@@ -1,74 +1,185 @@
 package com.example.regioes;
 
+import android.support.constraint.ConstraintLayout;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.view.View;
-import android.widget.Button;
+import android.widget.LinearLayout;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
-
-    private Button btnRegioes, btnEstados;
-    private Boolean clicou = true;
-    private String[] regioes = getResources().getStringArray(R.array.regioes);
-    private String[] regiaoNorte = getResources().getStringArray(R.array.regiaoNorte);
-    private String[] regiaoNordeste = getResources().getStringArray(R.array.regiaoNordeste);
-    private String[] regiaoCentroOeste = getResources().getStringArray(R.array.regiaoCentroOeste);
-    private String[] regiaoSudeste = getResources().getStringArray(R.array.regiaoSudeste);
-    private String[] regiaoSul = getResources().getStringArray(R.array.regiaoSul);
-
-    private Button estado;
-    private int contador, selecao = 0;
-    private int contRegioes, contEstados = 0;
+    private ConstraintLayout layout;
+    private TextView tvRegiao;
+    private TextView tvEstado;
+    private String[] regiao , sul , sudeste , centro_oeste , nordeste , norte;
+    private int contRegiao = 0;
+    private int contEstado = 0;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        btnRegioes = (Button) findViewById(R.id.btnRegioes);
-        btnEstados = (Button) findViewById(R.id.btnEstados);
 
-        btnRegioes.setOnClickListener(new View.OnClickListener() {
+        regiao = getResources().getStringArray(R.array.regioes);
+        sul = getResources().getStringArray(R.array.regiaoSul);
+        sudeste = getResources().getStringArray(R.array.regiaoSudeste);
+        centro_oeste = getResources().getStringArray(R.array.regiaoCentroOeste);
+        nordeste = getResources().getStringArray(R.array.regiaoNordeste);
+        norte = getResources().getStringArray(R.array.regiaoNorte);
+
+        layout = (ConstraintLayout) findViewById(R.id.swipe);
+        tvRegiao = (TextView) findViewById(R.id.txtRegiao);
+        tvEstado = (TextView) findViewById(R.id.txtEstado);
+
+        tvRegiao.setText( regiao[contRegiao] );
+        tvEstado.setText( norte[contEstado] );
+
+
+        layout.setOnTouchListener(new OnSwipeTouchListener(this){
             @Override
-            public void onClick(View view) {
-//                btnRegioes.setText(regioes[contador] );
-                contador++;
+            public void onSwipeTop() {
+                super.onSwipeTop();
 
+                if(contRegiao == regiao.length)
+                {
+                    contRegiao = 0;
+                }
+                else
+                {
+                    contRegiao++;
+                }
+                tvRegiao.setText(regiao[contRegiao]);
+                AtualizaEstadoRegioao();
+            }
+
+            @Override
+            public void onSwipeBottom() {
+                super.onSwipeBottom();
+
+                if(contRegiao == 0)
+                {
+                    contRegiao = regiao.length;
+                }
+                else
+                {
+                    contRegiao--;
+                }
+                tvRegiao.setText(regiao[contRegiao]);
+                AtualizaEstadoRegioao();
+            }
+
+
+
+            @Override
+            public void onSwipeRight() {
+                super.onSwipeRight();
+                switch (regiao[contRegiao]){
+                    case "Norte":
+                        tvEstado.setText(norte[contEstado]);
+                        if (contEstado >= norte.length){
+                            contEstado = 0;
+                        }
+                        break;
+
+                    case "Nordeste":
+                        tvEstado.setText(nordeste[contEstado]);
+                        if (contEstado >= nordeste.length){
+                            contEstado = 0;
+                        }
+                        break;
+
+                    case "Centro-Oeste":
+                        tvEstado.setText(centro_oeste[contEstado]);
+                        if (contEstado >= centro_oeste.length){
+                            contEstado = 0;
+                        }
+                        break;
+
+                    case "Sudeste":
+                        tvEstado.setText(sudeste[contEstado]);
+                        if (contEstado >= sudeste.length){
+                            contEstado = 0;
+                        }
+                        break;
+
+                    case "Sul":
+                        tvEstado.setText(sul[contEstado]);
+                        if (contEstado >= sul.length){
+                            contEstado = 0;
+                        }
+                        break;
+                }
+                contEstado++;
+            }
+
+            @Override
+            public void onSwipeLeft() {
+                super.onSwipeLeft();
+                switch (regiao[contRegiao]){
+                    case "Norte":
+                        tvEstado.setText(norte[contEstado]);
+                        if (contEstado == 0){
+                            contEstado = norte.length;
+                        }
+                        break;
+
+                    case "Nordeste":
+                        tvEstado.setText(nordeste[contEstado]);
+                        if (contEstado == 0){
+                            contEstado = nordeste.length;
+                        }
+                        break;
+
+                    case "Centro-Oeste":
+                        tvEstado.setText(centro_oeste[contEstado]);
+                        if (contEstado == 0){
+                            contEstado = centro_oeste.length;
+                        }
+                        break;
+
+                    case "Sudeste":
+                        tvEstado.setText(sudeste[contEstado]);
+                        if (contEstado == 0){
+                            contEstado = sudeste.length;
+                        }
+                        break;
+
+                    case "Sul":
+                        tvEstado.setText(sul[contEstado]);
+                        if (contEstado == 0){
+                            contEstado = sul.length;
+                        }
+                        break;
+                }
+                contEstado--;
+            }
+
+            public void AtualizaEstadoRegioao()
+            {
+                contEstado = 0;
+                switch (regiao[contRegiao]){
+                    case "Norte":
+                        tvEstado.setText(norte[contEstado]);
+                        break;
+
+                    case "Nordeste":
+                        tvEstado.setText(nordeste[contEstado]);
+                        break;
+
+                    case "Centro-Oeste":
+                        tvEstado.setText(centro_oeste[contEstado]);
+                        break;
+
+                    case "Sudeste":
+                        tvEstado.setText(sudeste[contEstado]);
+                        break;
+
+                    case "Sul":
+                        tvEstado.setText(sul[contEstado]);
+                        break;
+                }
             }
         });
 
-//        btnRegioes.setOnTouchListener( new OnSwipeTouchListener(this){
-//
-//            @Override
-//            public void onSwipeRight() {
-//                super.onSwipeRight();
-//                if(){
-//                    selecao ++;
-//                }
-//                contador ++;
-//                tvAfirmacao.setText( afirmacoes[contador]);
-//            }
-//
-//            @Override
-//            public void onSwipeLeft() {
-//                super.onSwipeLeft();
-//                if( !gabarito[contador]){
-//                    acertos ++;
-//                }
-//                contador ++;
-//                tvAfirmacao.setText( afirmacoes[contador]);
-//            }
-//
-//            @Override
-//            public void onSwipeTop() {
-//                super.onSwipeTop();
-//            }
-//
-//            @Override
-//            public void onSwipeBottom() {
-//                super.onSwipeBottom();
-//            }
-//        });
     }
-
 }
